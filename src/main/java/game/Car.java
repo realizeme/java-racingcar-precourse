d05755ac41f7ba356f8e45d43dfcff08a39bafa2
hyunsung_kim <hyunsung.kim@superb-ai.com>
2021-05-02 04:41:59 +0900

    feat(car): add car class
    Added Car class
    - move method in Car

--- src/main/java/game/Car.java
+++ src/main/java/game/Car.java
@@ -1,2 +1,28 @@
-package game;public class Car {
+package game;
+
+import util.RandomNumber;
+
+public class Car {
+    public static int MOVING_INDCATTOR = 4;
+
+    private String name;
+    private int position;
+
+    public Car(String name, int position){
+        this.name = name;
+        this.position = position;
+    }
+
+    public boolean move(RandomNumber randomNumber) {
+        if(randomNumber.isGreaterEqualThan(MOVING_INDCATTOR)){
+            forward();
+            return true;
+        }
+        return false;
+    }
+
+    public void forward() {
+        position++;
+    }
+
 }
--- src/test/java/game/CarTest.java
+++ src/test/java/game/CarTest.java
@@ -1,2 +1,26 @@
-package game;public class CarTest {
+package game;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import util.RandomNumber;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+    @Test
+    @DisplayName("자동차_멈춤")
+    public void 자동차_멈춤_확인(){
+        Car car = new Car("1번 자동차", 0);
+        assertThat(car.move(new RandomNumber(0))).isFalse();
+        assertThat(car.move(new RandomNumber(3))).isFalse();
+    }
+
+    @Test
+    @DisplayName("자동차_이동")
+    public void 자동차_이동_확인(){
+        Car car = new Car("1번 자동차", 0);
+        assertThat(car.move(new RandomNumber(4))).isTrue();
+        assertThat(car.move(new RandomNumber(9))).isTrue();
+    }
 }
